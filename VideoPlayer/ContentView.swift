3764687c02754f16b0ca210ac7e564aa06ab2671
taihsinlee <taihsinlee.tw@gmail.com>
2024-10-02 16:47:30 +0800

    [feat]: Implement basic SwiftUI VideoPlayer

--- VideoPlayer/ContentView.swift
+++ VideoPlayer/ContentView.swift
@@ -6,15 +6,16 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+	@State var player = AVPlayer(url: URL(string: "https://media.w3.org/2010/05/sintel/trailer.mp4")!)
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+        VStack {			
+			VideoPlayer(player: player)
+				.frame(height: 400)
+		}
         .padding()
     }
 }
