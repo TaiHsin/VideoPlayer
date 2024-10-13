57828597aa07d4f3963c0619b56ba29622e94661
taihsinlee <taihsinlee.tw@gmail.com>
2024-10-13 12:44:37 +0800

    [feat]: Introduce URLInputTextFieldViewModel and refactor URLInputTextField codes

---
+++ VideoPlayer/Model/URLInputTextFieldViewModel.swift
@@ -0,0 +1,50 @@
+//
+//  URLInputTextFieldViewModel.swift
+//  VideoPlayer
+//
+//  Created by Taihsin L on 2024/10/13.
+//
+
+import Foundation
+import Combine
+import UIKit
+
+struct TextFieldInput {
+	let buttonDidTapSubject = PassthroughSubject<Void, Never>()
+	let textChanged = PassthroughSubject<String, Never>()
+}
+
+class TextFieldOutput: ObservableObject {
+	@Published var urlText: String = ""
+	@Published var selectedVideoURL: URL?
+}
+
+class TextFieldViewModel {
+	let input = TextFieldInput()
+	let output = TextFieldOutput()
+	
+	private let application: UIApplication
+	private var cancellables = Set<AnyCancellable>()
+	
+	init(application: UIApplication = .shared) {
+		self.application = application
+
+		input.textChanged
+			.assign(to: \.urlText, on: output)
+			.store(in: &cancellables)
+		
+		input.buttonDidTapSubject
+			.combineLatest(output.$urlText)
+			.map { [application] (_, urlText) -> URL? in
+				guard let url = URL(string: urlText),
+					  application.canOpenURL(url) else {
+					// TODO: Error handling if need
+					print("Invalid URL input")
+					return nil
+				}
+				return url
+			}
+			.assign(to: \.selectedVideoURL, on: output)
+			.store(in: &cancellables)
+	}
+}
--- VideoPlayer/View/URLInputTextField.swift
+++ VideoPlayer/View/URLInputTextField.swift
@@ -8,30 +8,38 @@
 import SwiftUI
 
 struct URLInputTextField: View {
-	@State var text: String = ""
+	private let viewModel: TextFieldViewModel
+	@ObservedObject var output: TextFieldOutput
+	
+	init(viewModel: TextFieldViewModel = .init()) {
+		self.viewModel = viewModel
+		self.output = viewModel.output
+	}
 
 	var body: some View {
 
 		ZStack(alignment: .bottom) {
-			TextField(" Input video url and play", text: $text, axis: .vertical)
-				.padding(.trailing, 25)
-				.padding(9)
-				.background(
-					RoundedRectangle(cornerRadius: 25)
-						.stroke(lineWidth: 1.0)
-						.foregroundStyle(Color(uiColor: .clear))
-						.background(
-							Color.gray.opacity(0.1)
-								.clipShape(RoundedRectangle(cornerRadius: 25))
-						)
-				)
+			TextField(" Input video url and play",
+					  text: Binding(
+						get: { output.urlText },
+						set: { viewModel.input.textChanged.send($0) }
+					  ),
+					  axis: .vertical)
+			.padding(.trailing, 25)
+			.padding(9)
+			.background(
+				RoundedRectangle(cornerRadius: 25)
+					.stroke(lineWidth: 1.0)
+					.foregroundStyle(Color(uiColor: .clear))
+					.background(Color.gray.opacity(0.1)
+						.clipShape(RoundedRectangle(cornerRadius: 25)))
+			)
 
 			HStack(alignment: .bottom) {
 				Spacer()
 				
 				Button {
-					// TODO: Button click action
-					print("Button clicked")
+					viewModel.input.buttonDidTapSubject.send()
 				} label: {
 					Image(systemName: "play.circle")
 						.foregroundColor(Color(uiColor: .systemBlue))
@@ -40,6 +48,18 @@ struct URLInputTextField: View {
 				}
 			}
 			.padding(.bottom, 5)
+			.fullScreenCover(isPresented: Binding<Bool>(
+				get: { viewModel.output.selectedVideoURL != nil },
+				set: { isPresented in
+					if !isPresented {
+						viewModel.input.textChanged.send("")
+					}
+				}
+			)) {
+				if let videoURL = viewModel.output.selectedVideoURL {
+					PlayerView(viewModel: .init(video: .init(id: .init(), url: videoURL, title: "yoyoyo")))
+				}
+			}
 		}
 		.padding(10)
     }
